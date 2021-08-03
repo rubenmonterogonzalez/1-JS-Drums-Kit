d0a0d0c444c3c7161de9b57385a6be42a15e399e
Ruben Montero <rubenmontero@rocketmail.com>
2021-08-03 12:00:04 +0200

    JAVASCRIPT DRUMS KIT

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// BINDING SOUNDS WITH KEYS
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.toggle("playing");
+}
+
+window.addEventListener("keydown", playSound);
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") {
+    return;
+  }
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
